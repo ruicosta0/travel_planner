0065ad9fe40037974e5dc9c1fae5dd646c196657
ruicosta0 <62068297+ruicosta0@users.noreply.github.com>
2021-10-15 11:49:07 +0100

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,15 @@
+import { performAction } from './js/app'
+import { postTrip } from './js/postTrip'
+
+import './styles/style.scss'
+import './styles/header.scss'
+import './styles/button.scss'
+import './styles/image.scss'
+import './styles/counter.scss'
+import './styles/displayFields.scss'
+import './styles/footer.scss'
+
+export { 
+    performAction,
+    postTrip
+    }
